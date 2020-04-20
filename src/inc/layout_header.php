53d975a754eace033ec4212dbd151a5a9b718f6d
magicbrothers <felix@felix-zauberer.de>
2020-04-20 14:37:33 +0200

    [~] import fix

--- src/inc/layout_header.php
+++ src/inc/layout_header.php
@@ -21,7 +21,7 @@
 <html lang="de">
 <head>
 	<title><?=$header." - ".$page_title?></title>
-	<link rel="stylesheet" href="<?=$base?>inc/style.css" />
+	<link rel="stylesheet" href="<?=$base?>magicWebTemplate/src/inc/style.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="UTF-8" />
 </head>
